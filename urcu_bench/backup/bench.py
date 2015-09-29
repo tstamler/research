b66063eefccc9ab5602246a3de53acdda64fe2c5
tstamler <timstamler@gmail.com>
2015-09-29 14:09:11 -0400

    added scripts

---
+++ urcu_bench/backup/bench.py
@@ -0,0 +1,19 @@
+import os, time, re, string
+from time import gmtime, strftime
+
+input_trace = [ "ll_0p_key", "ll_10p_key", "ll_20p_key", "ll_30p_key","ll_40p_key","ll_50p_key",
+                "ll_60p_key", "ll_70p_key", "ll_80p_key", "ll_90p_key","ll_100p_key" ]
+num_cpu = ["1", "10", "20", "30", "40"]
+
+outputfile = "log_"+ strftime("%Y-%m-%d_%H_%M_%S", gmtime())
+
+for trace in input_trace:
+    for ncpu in num_cpu:
+        os.system("echo \"#define NUM_CPU ("+ncpu+")\" > config.h")
+        os.system("echo \"#define TRACE_FILE \\\"../ll_trace/"+trace+"\\\"\" >> config.h")
+        if trace == "":
+            os.system("echo \"#define READ_ONLY\" >> config.h")
+
+        os.system("rm ./bench; make; ./bench >> "+outputfile)
+        time.sleep(2)
+        exit()
---
+++ urcu_bench/config.h
@@ -0,0 +1,2 @@
+#define NUM_CPU (20)
+#define TRACE_FILE "../ll_trace/ll_80p_key"
